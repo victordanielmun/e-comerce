dbfe9666a4d0c607e5fe91ec56e6b08d2e87c637
victordanielmun <victordanielmun@gmail.com>
2023-02-11 20:55:50 -0500

    Add Pages for Router-Dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import ForgotPassword from './pages/ForgotPassword';
+import Offer from './pages/Offer';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
-    <div className="App">
-        <h1 className='text-2xl '>Hello world</h1>
-    </div>
+
+    <Router>
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/ForgotPassword" element={<ForgotPassword />} />
+          <Route path="/Offer" element={<Offer />} />
+          <Route path="/Profile" element={<Profile />} />
+          <Route path="/SignIn" element={<SignIn />} />
+          <Route path="/SignUp" element={<SignUp />} />
+          
+          
+      </Routes>
+    </Router>
+
+
   );
 }
 
